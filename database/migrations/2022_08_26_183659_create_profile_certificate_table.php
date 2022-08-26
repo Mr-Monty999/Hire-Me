b08557ae2a66f1f6baa06f5d0b5ce4a263928dd0
Mr-Monty999 <montser.mohmed2022@gmail.com>
2022-08-26 20:52:14 +0200

    Fix Migration

--- database/migrations/2022_08_26_183659_create_profile_certificate_table.php
+++ database/migrations/2022_08_26_183659_create_profile_certificate_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateprofileCertificationTable extends Migration
+class CreateProfileCertificateTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateprofileCertificationTable extends Migration
      */
     public function up()
     {
-        Schema::create('profile_certification', function (Blueprint $table) {
+        Schema::create('profile_certificate', function (Blueprint $table) {
             $table->bigInteger("profile_id")->unsigned();
-            $table->bigInteger("certification_id")->unsigned();
+            $table->bigInteger("certificate_id")->unsigned();
             $table->timestamps();
 
-            $table->primary(["profile_id", "certification_id"]);
+            $table->primary(["profile_id", "certificate_id"]);
             $table->foreign("profile_id")->references("id")->on("profiles")->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreign("certification_id")->references("id")->on("certificates")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign("certificate_id")->references("id")->on("certificates")->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
