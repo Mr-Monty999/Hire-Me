cafe96ede0de8d59f34daccdd31ee2a76d238479
Mr-Monty999 <montser.mohmed2022@gmail.com>
2022-08-28 09:44:58 +0200

    Add Seeders & Factories

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Profile;
 
 class Certificate extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["name", "company_name", "company_id"];
+
+
+    // public function be
+    // {
+    // }
 }
